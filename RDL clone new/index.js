466a611b38b28ef072fb726741e0884700451b97
Kadeejath Salaha <codestudy475@gmail.com>
2024-08-25 19:56:39 +0530

    Initial Intern branch commit

---
+++ RDL clone new/index.js
@@ -0,0 +1,13 @@
+function toggleSearch() {
+    var searchBox = document.querySelector('.search-box');
+    searchBox.classList.toggle('active');
+}
+
+function search() {
+    // Code to handle search functionality
+}
+
+function cancel() {
+    var searchBox = document.querySelector('.search-box');
+    searchBox.classList.remove('active');
+}
